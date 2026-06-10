89370c7ccaebd539eba2770e57c2f1c4ccd4b6c9
danishpu04 <utamadanish04@gmail.com>
2026-06-10 07:30:38 +0700

    1st commit

---
+++ app/Views/v_contact.php
@@ -0,0 +1,6 @@
+<?= $this->extend('layout') ?>
+<?= $this->section('content') ?>
+
+<p class="mb-4">Lorem ipsum dolor, sit amet consectetur adipisicing elit. Dolor harum fugit commodi quaerat, saepe labore. Totam neque tempora molestias, officiis, error, ipsum minus sunt suscipit amet molestiae nemo voluptate quibusdam non qui sequi? Ducimus ullam est soluta nihil repellat delectus nesciunt, nam fugit, dolor minima temporibus voluptatem quasi officia quibusdam at eos illum, voluptatum quidem. Unde rerum fugit totam quas expedita? Tempore placeat ad provident tempora quam facilis vitae, recusandae consequatur nam obcaecati cum quisquam reprehenderit eos vel dolorem ipsa! Exercitationem cumque rerum obcaecati id vero corporis vel neque eos excepturi, sint quidem laboriosam expedita nobis sequi commodi molestiae voluptate.</p>
+
+<?= $this->endSection() ?>
