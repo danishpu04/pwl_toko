16c2ef5bbb50923f564bca32a7c6c7b4f71f089e
danishpu04 <utamadanish04@gmail.com>
2026-06-24 08:51:37 +0700

    upload hasil tutorial webservice client

--- app/Views/components/sidebar.php
+++ app/Views/components/sidebar.php
@@ -53,6 +53,14 @@
       <?php
       }
       ?>
+
+      <li class="nav-item">
+        <a class="nav-link <?php echo (uri_string() == 'history') ? "" : "collapsed" ?>" href="history">
+          <i class="bi bi-person"></i>
+          <span>History</span>
+        </a>
+      </li><!-- End History Nav -->
+      
       <li class="nav-item">
         <a class="nav-link <?php echo (uri_string() == 'contact') ? "" : "collapsed" ?>" href="contact">
           <i class="bi bi-receipt"></i>
